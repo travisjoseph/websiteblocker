f7fa477b5617e67fae5c4344d50fccd7e2e900b6
Travis Joseph <travis.joseph94@gmail.com>
2018-01-30 08:50:13 -0800

    Cleaned up code

--- websiteblocker.py
+++ websiteblocker.py
@@ -1,13 +1,12 @@
 import time
 from datetime import datetime as dt
 
-hostsPath_temp="hosts"
 hostsPath="/etc/hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.reddit.com","reddit.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day, 9) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day, 16):
+    if dt(dt.now().year,dt.now().month,dt.now().day, 8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day, 16):
         with open(hostsPath, 'r+') as file:
             content=file.read()
             for website in website_list:
