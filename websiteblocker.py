544a75918999690cae78253e24b3780c1420615c
Travis Joseph <travis.joseph94@gmail.com>
2018-01-31 18:32:47 -0800

    Created GUI for start and end time. No functionality

--- websiteblocker.py
+++ websiteblocker.py
@@ -1,9 +1,17 @@
 import time
+from tkinter import *
+import gui
 from datetime import datetime as dt
 
 hostsPath="/etc/hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.reddit.com","reddit.com"]
+
+root = Tk()
+root.title("Website Blocker")
+gui.makeGUI(root)
+root.mainloop()
+
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day, 8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day, 16):
