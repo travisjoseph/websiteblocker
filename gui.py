42a3af1a1a979489cad4e2bcc02689261fc184b8
Travis Joseph <travis.joseph94@gmail.com>
2018-01-31 20:19:28 -0800

    still messing arounf with Tkinter

--- gui.py
+++ gui.py
@@ -4,35 +4,37 @@ hoursList = ["00", "01", "03", "04", "05", "06", "07", "08", "09", "10", "11",
              "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
 minutesList = ["00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"]
 
-def makeGUI(root):
+def GUI(root):
+    def getTimes():
+        return startHour.get() + startMinute.get()
+
     #START TIME
     lblStart = Label(root, text = "Start Time: ")
     startHour = StringVar()
     startHour.set(hoursList[0])
-    startHour = OptionMenu(root, startHour, *hoursList)
+    om_startHour = OptionMenu(root, startHour, *hoursList)
     startMinute = StringVar()
     startMinute.set(minutesList[0])
-    startMinute = OptionMenu(root, startMinute, *minutesList)
+    om_startMinute = OptionMenu(root, startMinute, *minutesList)
 
 
     #END TIME
     lblEnd = Label(root, text = "End Time: ")
     endHour = StringVar()
     endHour.set(hoursList[0])
-    endHour = OptionMenu(root, endHour, *hoursList)
+    om_endHour = OptionMenu(root, endHour, *hoursList)
     endMinute = StringVar()
     endMinute.set(hoursList[0])
-    endMinute = OptionMenu(root, endMinute, *hoursList)
+    om_endMinute = OptionMenu(root, endMinute, *hoursList)
 
     #APPLY TIME
-    btnApply = Button(root, text = "Apply")
-
+    btnApply = Button(root, text = "Apply", command = getTimes)
 
     #GRID LAYOUT
     lblStart.grid(row=0, column=0)
-    startHour.grid(row=0, column=1)
-    startMinute.grid(row=0, column=3)
+    om_startHour.grid(row=0, column=1)
+    om_startMinute.grid(row=0, column=3)
     lblEnd.grid(row=0, column=4)
-    endHour.grid(row=0, column=5)
-    endMinute.grid(row=0, column=6)
+    om_endHour.grid(row=0, column=5)
+    om_endMinute.grid(row=0, column=6)
     btnApply.grid(row=1, column=3)
--- websiteblocker.py
+++ websiteblocker.py
@@ -1,21 +1,59 @@
 import time
 from tkinter import *
-import gui
 from datetime import datetime as dt
 
 hostsPath="/etc/hosts"
+temp_host = "hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.reddit.com","reddit.com"]
+hoursList = ["00", "01", "03", "04", "05", "06", "07", "08", "09", "10", "11",
+             "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
+minutesList = ["00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"]
+
 
 root = Tk()
 root.title("Website Blocker")
-gui.makeGUI(root)
+
+def getTimes():
+    print(int(startHour.get()))
+
+#START TIME
+lblStart = Label(root, text = "Start Time: ")
+startHour = StringVar()
+startHour.set(hoursList[0])
+om_startHour = OptionMenu(root, startHour, *hoursList)
+startMinute = StringVar()
+startMinute.set(minutesList[0])
+om_startMinute = OptionMenu(root, startMinute, *minutesList)
+
+#END TIME
+lblEnd = Label(root, text = "End Time: ")
+endHour = StringVar()
+endHour.set(hoursList[0])
+om_endHour = OptionMenu(root, endHour, *hoursList)
+endMinute = StringVar()
+endMinute.set(hoursList[0])
+om_endMinute = OptionMenu(root, endMinute, *hoursList)
+
+#APPLY TIME
+btnApply = Button(root, text = "Apply", command = getTimes)
+
+#GRID LAYOUT
+lblStart.grid(row=0, column=0)
+om_startHour.grid(row=0, column=1)
+om_startMinute.grid(row=0, column=3)
+lblEnd.grid(row=0, column=4)
+om_endHour.grid(row=0, column=5)
+om_endMinute.grid(row=0, column=6)
+btnApply.grid(row=1, column=3)
+
 root.mainloop()
 
 
+
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day, 8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day, 16):
-        with open(hostsPath, 'r+') as file:
+    if dt(dt.now().year,dt.now().month,dt.now().day, 14) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day, 9):
+        with open(temp_host, 'r+') as file:
             content=file.read()
             for website in website_list:
                 if website in content:
@@ -24,7 +62,7 @@ while True:
                     file.write(redirect + " " + website + "\n")
         time.sleep(300)
     else:
-        with open(hostsPath, 'r+') as file:
+        with open(temp_host, 'r+') as file:
             content=file.readlines()
             file.seek(0)
             for line in content:
