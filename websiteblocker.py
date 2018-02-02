779bbddb1e11fef7efdb78dea836bbd9fea0018f
Travis Joseph <travisjoseph@frank.local>
2018-02-01 16:45:35 -0800

    Added first parts of GUI. No functionality.

--- websiteblocker.py
+++ websiteblocker.py
@@ -4,9 +4,47 @@ from tkinter import *
 import gui
 
 tmpHosts="hosts"
-hostsPath="/etc/hosts"
+#hostsPath="/etc/hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.reddit.com","reddit.com"]
+time_list=['00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00',
+           '03:30', '04:00', '04:30', '05:00', '05:30', '06:00', '06:30',
+           '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00',
+           '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30',
+           '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00',
+           '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30',
+           '21:00', '21:30', '22:00', '22:30', '23:00', '23:30']
+
+#GUI
+window = Tk()
+window.title("Website Blocker")
+
+lbl_StartTime=Label(window, text="Start Time: ")
+lbl_StartTime.grid(row=0, column=0)
+StartTime = StringVar()
+StartTime.set(time_list[0])
+om_StartTime = OptionMenu(window, StartTime, *time_list)
+om_StartTime.grid(row=0, column=1)
+
+lbl_EndTime=Label(window, text="End Time: ")
+lbl_EndTime.grid(row=0, column=2)
+EndTime = StringVar()
+EndTime.set(time_list[0])
+om_EndTime = OptionMenu(window, EndTime, *time_list)
+om_EndTime.grid(row=0, column=3)
+
+lbl_blocked=Label(window, text="Blocked Website List")
+lbl_blocked.grid(row=1, column=0)
+
+lbl_redirect=Label(window, text="Redirect Address: ")
+lbl_redirect.grid(row=1, column=2)
+
+siteList=StringVar()
+lst_siteList=Listbox(window, height=6, width=30)
+lst_siteList.grid(row=2, column=0, rowspan=3, columnspan=3)
+
+window.mainloop()
+
 
 
 while True:
